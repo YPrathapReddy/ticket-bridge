c87a7758987132b42f34c13c04d75807a3c391ef
PrathapReddy <yprathapreddy63015@gmail.com>
2026-06-28 01:39:39 +0530

    Save uploaded tickets to local storage

--- frontend/js/uploadTicket.js
+++ frontend/js/uploadTicket.js
@@ -1,4 +1,6 @@
 console.log("JavaScript Connected")
+
+
 let uploadForm = document.getElementById("uploadForm");
 
 uploadForm.addEventListener("submit", function(event){
@@ -13,8 +15,37 @@ uploadForm.addEventListener("submit", function(event){
     }
     else{
         alert("Resale price cannot be greater than original price.");
+        return;
         
     }
+
+    let ticketType = document.getElementById("ticketType").value;
+    let ticketNumber = document.getElementById("ticketNumber").value;
+    let fromPlace = document.getElementById("fromPlace").value;
+    let toPlace = document.getElementById("toPlace").value;
+    let travelDate = document.getElementById("travelDate").value;
+    let operator = document.getElementById("operator").value;
+    let seatNumber = document.getElementById("seatNumber").value;
+    let description = document.getElementById("description").value;
+
+    let currentTicket = {
+        ticketType : ticketType,
+        ticketNumber : ticketNumber,
+        fromPlace : fromPlace,
+        toPlace : toPlace,
+        travelDate : travelDate,
+        operator : operator,
+        seatNumber : seatNumber,
+        originalPrice : originalPrice,
+        resalePrice : resalePrice,
+        description : description
+    };
+    let tickets = JSON.parse(localStorage.getItem("tickets")) || [];
+    tickets.push(currentTicket);
+    localStorage.setItem("tickets", JSON.stringify(tickets));
+    console.log(tickets);
+    
+    
 });
 
 let ticketImage = document.getElementById("ticketImage");
